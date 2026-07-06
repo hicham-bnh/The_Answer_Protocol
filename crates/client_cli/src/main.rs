3156f6db3b7974656768f74199c3f406e08f6e47
hicham-bnh <benhabibmohammedhicham@gmail.com>
2026-07-06 03:48:53 +0200

    feat/server : Command Connect management and setup complete.

--- crates/client_cli/src/main.rs
+++ crates/client_cli/src/main.rs
@@ -14,24 +14,21 @@ fn main() {
             return;
         }
     };
-
     let stream_clone = stream.try_clone().expect("clone failed");
     let mut reader = BufReader::new(stream_clone);
-
     // Lire la greeting envoyée par le serveur
     let mut greeting = String::new();
     reader.read_line(&mut greeting).expect("erreur de lecture greeting");
-    print!("Serveur dit: {}", greeting);
+    print!("S: {}", greeting);
+    //// Envoyer une commande CONNECT
+    //let cmd = "TEST alice\n";
+    //stream.write_all(cmd.as_bytes()).expect("erreur d'écriture");
+    //println!("Envoyé: {}", cmd.trim());
 
-    // Envoyer une commande CONNECT
-    let cmd = "LOOK alice\n";
-    stream.write_all(cmd.as_bytes()).expect("erreur d'écriture");
-    println!("Envoyé: {}", cmd.trim());
-
-    // Lire la réponse à CONNECT
-    let mut response = String::new();
-    reader.read_line(&mut response).expect("erreur de lecture réponse");
-    print!("Serveur dit: {}", response);
+    //// Lire la réponse à CONNECT
+    //let mut response = String::new();
+    //reader.read_line(&mut response).expect("erreur de lecture réponse");
+    //print!("Serveur dit: {}", response);
 
     // Boucle interactive : tape des commandes au clavier
     let stdin = std::io::stdin();
@@ -39,22 +36,18 @@ fn main() {
         let mut input = String::new();
         stdin.read_line(&mut input).expect("erreur stdin");
         let input = input.trim();
-
         if input.is_empty() {
             continue;
         }
-
         stream.write_all(format!("{}\n", input).as_bytes()).expect("erreur d'écriture");
-
-        if input.eq_ignore_ascii_case("QUIT") {
-            break;
-        }
-
         let mut response = String::new();
         if reader.read_line(&mut response).unwrap_or(0) == 0 {
             println!("Serveur déconnecté.");
             break;
         }
-        print!("Serveur dit: {}", response);
+        print!("S: {}", response);
+        if response == "OK bye\n" {
+            break;
+        }
     }
 }
