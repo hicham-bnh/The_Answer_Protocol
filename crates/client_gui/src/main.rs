0adbd3b3ff4daeb416412946be581b90bd3221cc
Matthieu Dourdoigne <mdourdoi@z3r9p5.42lyon.fr>
2026-08-05 20:14:39 +0200

    wip

--- crates/client_gui/src/main.rs
+++ crates/client_gui/src/main.rs
@@ -1,10 +1,32 @@
+use eframe::egui;
+
 struct TapClient {
-    server_adress: String
+    server_address: String,
 }
 
-fn main(){
-    let client = TapClient{
-        server_adress: String::from("127.0.0.1:8080")
-    };
-    println!("Adresse du serveur : {}", client.server_adress);
+impl TapClient {
+    fn new(address: String) -> TapClient {
+        TapClient {
+            server_address: address,
+        }
+    }
+
+    fn describe(&self) {
+        println!("{}", self.server_address)
+    }
+}
+
+impl eframe::App for TapClient {
+    fn ui(&mut self, ui: &mut egui::Ui, _frame: &mut eframe::Frame) {
+        ui.heading("TAP client");
+    }
+}
+
+fn main() -> eframe::Result {
+    let options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "TAP",
+        options,
+        Box::new(|_cc| Ok(Box::new(TapClient::new("127.0.0.1:8080".to_string())))),
+    )
 }
