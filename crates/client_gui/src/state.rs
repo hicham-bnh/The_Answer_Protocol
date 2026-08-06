8e319882ae8dd0fb2b922c663077f2b9512d21f2
Matthieu Dourdoigne <mdourdoi@z3r9p5.42lyon.fr>
2026-08-06 02:21:45 +0200

    visual done, need to do the network part

---
+++ crates/client_gui/src/state.rs
@@ -0,0 +1,25 @@
+use std::collections::HashMap;
+
+pub struct Room {
+    pub id: String,
+    pub name: String,
+    pub description: String,
+    pub exits: HashMap<String, String>,
+}
+
+#[derive(Default, PartialEq, Clone)]
+pub enum ChatTab {
+    Global,
+    #[default]
+    Room,
+    Group,
+}
+
+pub fn prettify(id: &str) -> String {
+    for prefix in ["loc.", "item.", "npc."] {
+        if let Some(rest) = id.strip_prefix(prefix) {
+            return rest.replace("_", " ");
+        }
+    }
+    id.to_string()
+}
